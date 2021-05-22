cc33192ae642d785ce4b42c447a68a87a90390a3
LidiaStrelets <0630610171li@gmail.com>
2021-05-22 11:05:51 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import debounce from 'lodash.debounce';
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
 import fetchCountries from './js/fetchCountries.js';
 import listTpl from './templates/countries-list.hbs';
 import countryTpl from './templates/specificCountry.hbs';
+
+const { error } = require('@pnotify/core');
+const { defaults } = require('@pnotify/core');
+
+defaults.type = 'error';
+defaults.sticker = false;
+defaults.delay = 3000;
 
 const refs = {
   input: document.querySelector('#filter'),
@@ -10,6 +19,7 @@ const refs = {
   container: document.querySelector('.js-container'),
 };
 let markUp = null;
+
 refs.input.addEventListener('input', debounce(handleInput, 500));
 
 function handleInput(event) {
@@ -18,7 +28,9 @@ function handleInput(event) {
   fetchCountries(query)
     .then(data => {
       if (data.length > 10) {
-        alert('Too many matches found! Please, enter a more specific query!');
+        const tooManyMatches = error({
+          text: 'Too many matches found! Please, enter a more specific query!',
+        });
       } else if (2 <= data.length && data.length <= 10) {
         markUp = listTpl(data);
         refs.container.innerHTML = markUp;
@@ -27,8 +39,10 @@ function handleInput(event) {
         refs.container.innerHTML = markUp;
       }
     })
-    .catch(error => {
-      alert(error);
+    .catch(e => {
+      const tooManyMatches = error({
+        text: 'Please, enter the correct country name!',
+      });
     })
     .finally(() => {
       refs.form.reset();
