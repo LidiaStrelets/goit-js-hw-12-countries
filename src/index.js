186021abd235d2e4477dfe797a41dbab725a0218
LidiaStrelets <0630610171li@gmail.com>
2021-05-22 09:55:43 +0300

    main part done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import debounce from 'lodash.debounce';
 import './sass/main.scss';
+import fetchCountries from './js/fetchCountries.js';
+import listTpl from './templates/countries-list.hbs';
+import countryTpl from './templates/specificCountry.hbs';
+
+const refs = {
+  input: document.querySelector('#filter'),
+  form: document.querySelector('.js-form'),
+  container: document.querySelector('.js-container'),
+};
+let markUp = null;
+refs.input.addEventListener('input', debounce(handleInput, 500));
+
+function handleInput(event) {
+  const query = event.target.value;
+
+  fetchCountries(query)
+    .then(data => {
+      if (data.length > 10) {
+        alert('Too many matches found! Please, enter a more specific query!');
+      } else if (2 <= data.length && data.length <= 10) {
+        markUp = listTpl(data);
+        refs.container.innerHTML = markUp;
+      } else {
+        markUp = countryTpl(...data);
+        refs.container.innerHTML = markUp;
+      }
+    })
+    .catch(error => {
+      alert(error);
+    })
+    .finally(() => {
+      refs.form.reset();
+    });
+}
