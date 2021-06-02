222eb941b244cfa6f0add79fabd2095cb440fa1c
LidiaStrelets <0630610171li@gmail.com>
2021-06-02 12:14:04 +0300

    made falldown list

--- src/index.js
+++ src/index.js
@@ -18,15 +18,18 @@ const refs = {
   input: document.querySelector('#filter'),
   form: document.querySelector('.js-form'),
   container: document.querySelector('.js-container'),
+  countryContainer: document.querySelector('.country-container'),
 };
 let markUp = null;
 
 refs.input.addEventListener('input', debounce(handleInput, 500));
 
 async function handleInput(event) {
+  refs.countryContainer.innerHTML = '';
   const query = event.target.value;
 
   if (isEmptyString(query)) {
+    refs.container.classList.remove('shown');
     error({
       text: 'Please, enter something!!!',
     });
@@ -35,20 +38,43 @@ async function handleInput(event) {
       const data = await fetchCountries(query);
 
       if (data.length > 10) {
+        refs.container.classList.remove('shown');
         const tooManyMatches = error({
           text: 'Too many matches found! Please, enter a more specific query!',
         });
-      } else if (2 <= data.length && data.length <= 10) {
+      }
+      // else if (2 <= data.length && data.length <= 10) {
+      //   markUp = listTpl(data);
+      //   refs.container.innerHTML = markUp;
+      // } else {
+      //   markUp = countryTpl(...data);
+      //   refs.container.innerHTML = markUp;
+      // }
+      else {
         markUp = listTpl(data);
         refs.container.innerHTML = markUp;
-      } else {
-        markUp = countryTpl(...data);
-        refs.container.innerHTML = markUp;
+        refs.container.classList.add('shown');
       }
     } catch {
       const tooManyMatches = error({
         text: 'Please, enter the correct country name!',
       });
     }
+  }
+}
+
+refs.container.addEventListener('click', handleCountryClick);
+
+async function handleCountryClick(event) {
+  const countryToShow = event.target.textContent;
+  refs.container.classList.remove('shown');
+  try {
+    const data = await fetchCountries(countryToShow);
+    markUp = countryTpl(...data);
+    refs.countryContainer.innerHTML = markUp;
+  } catch (er) {
+    const countryError = error({
+      text: er,
+    });
   }
 }
